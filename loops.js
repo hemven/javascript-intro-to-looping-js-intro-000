c8d9d7fb17e844afa47ce6cd15c55b7a288ddae4
Hema Venkata <hvenkata@live.com>
2019-06-21 02:06:28 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -29,5 +29,5 @@ function incrementVariable() {
 function doWhileLoop(number) {
   do {
   console.log("I run once regardless.");
-} while (incrementVariable() < number);
+} while (incrementVariable() <= number);
 }
