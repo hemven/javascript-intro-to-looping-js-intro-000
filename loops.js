8af1bd53b8aa70dc5d098bda3f331b2de578543a
Hema Venkata <hvenkata@live.com>
2019-06-21 01:47:12 +0000

    Automatically backed up by Learn

---
+++ loops.js
@@ -0,0 +1,17 @@
+function forLoop (array) {
+  for (let i = 1; i < 25; i++) {
+      if (i == 1) {
+        array.push(`I am ${i} strange loop.`)
+      } else {
+        array.push(`I am ${i} strange loop${i === 0 ? '' : 's'}.`)
+      }
+  }
+  return array
+}
+
+forLoop ([])
+
+
+function whileLoop (number) {
+
+}
